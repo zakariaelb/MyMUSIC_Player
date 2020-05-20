faf8197947a59ceb3f9af92431ac41c3f4cda47a
zelbatmi <zakari_a@icloud.com>
2020-05-20 14:21:50 +0000

    1.0V

--- app/src/main/java/digiplus/ma/mymusicplayer/MainActivity.java
+++ app/src/main/java/digiplus/ma/mymusicplayer/MainActivity.java
@@ -2,23 +2,27 @@ package digiplus.ma.mymusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.adkar);
         Button PlayButton = (Button) findViewById(R.id.Play);
         Button PauseButton = (Button) findViewById(R.id.Pause);
 
         PlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mediaPlayer.start();
 
                 Toast.makeText(MainActivity.this, "Play",Toast.LENGTH_LONG ).show();
             }
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         PauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer.pause();
                 Toast.makeText(MainActivity.this, "Pause",Toast.LENGTH_LONG ).show();
             }
         });
