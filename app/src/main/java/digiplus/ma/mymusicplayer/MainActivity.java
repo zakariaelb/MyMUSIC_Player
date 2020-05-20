6d1f3b83b1ab097acbe401ebb1eb959652dd9d23
zelbatmi <zakari_a@icloud.com>
2020-05-20 17:10:22 +0000

    1.0V

--- app/src/main/java/digiplus/ma/mymusicplayer/MainActivity.java
+++ app/src/main/java/digiplus/ma/mymusicplayer/MainActivity.java
@@ -23,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "I'm Done !",Toast.LENGTH_SHORT ).show();
+                    }
+                });
 
                 Toast.makeText(MainActivity.this, "Play",Toast.LENGTH_LONG ).show();
             }
